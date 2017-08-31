57463e443921913cf6d2bfd3c11a545c803ab74b
haniotak <h8XntUEBNae6>
2017-08-31 09:03:03 -0700

    provide port details through ethPortsForDevice

--- backend/src/main/java/net/es/oscars/web/rest/TopoController.java
+++ backend/src/main/java/net/es/oscars/web/rest/TopoController.java
@@ -2,21 +2,12 @@ package net.es.oscars.web.rest;
 
 
 import lombok.extern.slf4j.Slf4j;
-import net.es.oscars.resv.beans.PeriodBandwidth;
-import net.es.oscars.resv.db.FixtureRepository;
-import net.es.oscars.resv.db.PipeRepository;
-import net.es.oscars.resv.db.ScheduleRepository;
-import net.es.oscars.resv.db.VlanRepository;
-import net.es.oscars.resv.ent.*;
-import net.es.oscars.resv.enums.BwDirection;
-import net.es.oscars.resv.enums.Phase;
 import net.es.oscars.resv.svc.ResvLibrary;
 import net.es.oscars.resv.svc.ResvService;
-import net.es.oscars.topo.beans.IntRange;
 import net.es.oscars.topo.beans.PortBwVlan;
 import net.es.oscars.topo.db.DeviceRepository;
+import net.es.oscars.topo.ent.Port;
 import net.es.oscars.topo.enums.Layer;
-import net.es.oscars.topo.enums.UrnType;
 import net.es.oscars.topo.svc.TopoService;
 import net.es.oscars.web.beans.Interval;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,13 +40,13 @@ public class TopoController {
 
     @RequestMapping(value = "/api/topo/ethernetPortsByDevice", method = RequestMethod.GET)
     @ResponseBody
-    public Map<String, List<String>> ethernetPortsByDevice() {
-        Map<String, List<String>> result = new HashMap<>();
+    public Map<String, List<Port>> ethernetPortsByDevice() {
+        Map<String, List<Port>> result = new HashMap<>();
         deviceRepo.findAll().forEach(d -> {
-            List<String> ports = new ArrayList<>();
+            List<Port> ports = new ArrayList<>();
             d.getPorts().forEach(p -> {
                 if (p.getCapabilities().contains(Layer.ETHERNET)) {
-                    ports.add(p.getUrn());
+                    ports.add(p);
                 }
             });
             result.put(d.getUrn(), ports);
