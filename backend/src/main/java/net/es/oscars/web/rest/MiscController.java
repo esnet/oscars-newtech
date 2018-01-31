29f3a17c9b0c6712c186ef02b4fbdc39db2aaf4a
haniotak <h8XntUEBNae6>
2018-01-31 10:58:37 -0800

    add a version REST endpoint

---
+++ backend/src/main/java/net/es/oscars/web/rest/MiscController.java
@@ -0,0 +1,15 @@
+package net.es.oscars.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MiscController {
+
+    @RequestMapping(value = "/api/version", method = RequestMethod.GET)
+    public String getVersion() {
+        return "1.0.0-beta9";
+    }
+
+}
