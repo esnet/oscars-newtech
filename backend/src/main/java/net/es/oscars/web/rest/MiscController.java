1450c49e3a1c7033289c86b1fb13592f0e068d83
haniotak <h8XntUEBNae6>
2018-09-19 14:04:14 -0700

    ver bump

--- backend/src/main/java/net/es/oscars/web/rest/MiscController.java
+++ backend/src/main/java/net/es/oscars/web/rest/MiscController.java
@@ -17,7 +17,7 @@ import org.apache.commons.io.input.ReversedLinesFileReader;
 @RestController
 @Slf4j
 public class MiscController {
-    public static String version = "1.0.22";
+    public static String version = "1.0.23";
 
     @Value("${logging.file}")
     private String logfile;
