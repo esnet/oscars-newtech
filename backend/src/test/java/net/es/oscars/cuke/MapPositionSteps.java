e2436b2434b510af5babcc356f9ec1ff6f6606cb
sartaj10 <ssbaveja@es.net>
2019-07-06 11:48:37 -0700

    Add cuke tests for map position feature

--- backend/src/test/java/net/es/oscars/cuke/MapPositionSteps.java
+++ backend/src/test/java/net/es/oscars/cuke/MapPositionSteps.java
@@ -5,26 +5,21 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import lombok.extern.slf4j.Slf4j;
 import net.es.oscars.ctg.UnitTests;
-import net.es.oscars.dto.topo.DeviceModel;
 import net.es.oscars.topo.beans.DevicePositions;
 import net.es.oscars.topo.beans.MapNode;
 import net.es.oscars.topo.beans.Position;
 import net.es.oscars.topo.ent.Device;
-import net.es.oscars.topo.enums.DeviceType;
 import net.es.oscars.web.rest.MapController;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 @Slf4j
 @Category({UnitTests.class})
 public class MapPositionSteps extends CucumberSteps {
-    @Autowired
-    private Jackson2ObjectMapperBuilder builder;
-
     @Autowired
     private CucumberWorld world;
 
@@ -32,9 +27,7 @@ public class MapPositionSteps extends CucumberSteps {
     private MapController mapController;
 
     private List<MapNode> mapNodes = new ArrayList<>();
-
     private List<Device> devices = new ArrayList<>();
-
     private Map<String, Position> positionMap;
 
     @Given("^I load my positionMap from \"([^\"]*)\"$")
@@ -49,37 +42,21 @@ public class MapPositionSteps extends CucumberSteps {
         }
     }
 
-    @Given("^I add nodes for devices \"([^\"]*)\", \"([^\"]*)\" and \"([^\"]*)\"$")
-    public void my_devices(String a, String b, String c) {
+    @Given("^I load devices from \"([^\"]*)\"$")
+    public void my_devices(String path) {
+        File jsonFile = new File(path);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            devices = Arrays.asList(mapper.readValue(jsonFile, Device[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        Device aDevice = Device.builder()
-                .urn(a)
-                .model(DeviceModel.JUNIPER_MX)
-                .type(DeviceType.ROUTER)
-                .ipv4Address("10.0.0.1")
-                .build();
-
-        Device bDevice = Device.builder()
-                .urn(b)
-                .model(DeviceModel.JUNIPER_MX)
-                .type(DeviceType.ROUTER)
-                .ipv4Address("10.0.0.1")
-                .build();
-
-        Device cDevice = Device.builder()
-                .urn(c)
-                .model(DeviceModel.JUNIPER_MX)
-                .type(DeviceType.ROUTER)
-                .ipv4Address("10.0.0.1")
-                .build();
-
-
-        MapNode n1 = mapController.addNode(aDevice, positionMap);
-        MapNode n2 = mapController.addNode(bDevice, positionMap);
-        MapNode n3 = mapController.addNode(cDevice, positionMap);
+        MapNode n1 = mapController.addNode(devices.get(0), positionMap);
+        MapNode n2 = mapController.addNode(devices.get(1), positionMap);
+        MapNode n3 = mapController.addNode(devices.get(2), positionMap);
 
         Collections.addAll(mapNodes, n1, n2, n3);
-        Collections.addAll(devices, aDevice, bDevice, cDevice);
     }
 
     @Then("^I can verify my results$")
@@ -89,16 +66,31 @@ public class MapPositionSteps extends CucumberSteps {
         MapNode n3 = mapNodes.get(2);
 
         Device a = devices.get(0);
-        Device b = devices.get(0);
-        Device c = devices.get(0);
+        Device b = devices.get(1);
+        Device c = devices.get(2);
 
-        assert n1.getX().equals(positionMap.get(a.getUrn().split("-")[0]).getX());
-        assert n1.getY().equals(positionMap.get(a.getUrn().split("-")[0]).getY());
+        Integer n1_X = n1.getX();
+        Integer n1_Y = n1.getY();
+        Integer d1_X = positionMap.get(a.getUrn().split("-")[0]).getX();
+        Integer d1_Y = positionMap.get(a.getUrn().split("-")[0]).getY();
 
-        assert n2.getX().equals(positionMap.get(b.getUrn().split("-")[0]).getX());
-        assert n2.getY().equals(positionMap.get(b.getUrn().split("-")[0]).getY());
+        assert n1_X.equals(d1_X);
+        assert n1_Y.equals(d1_Y);
 
-        assert n3.getX().equals(positionMap.get(c.getUrn().split("-")[0]).getX());
-        assert n3.getY().equals(positionMap.get(c.getUrn().split("-")[0]).getY());
+        Integer n2_X = n2.getX();
+        Integer n2_Y = n2.getY();
+        Integer d2_X = positionMap.get(b.getUrn().split("-")[0]).getX();
+        Integer d2_Y = positionMap.get(b.getUrn().split("-")[0]).getY();
+
+        assert n2_X.equals(d2_X);
+        assert n2_Y.equals(d2_Y);
+
+        Integer n3_X = n3.getX();
+        Integer n3_Y = n3.getY();
+        Integer d3_X = positionMap.get(c.getUrn().split("-")[0]).getX();
+        Integer d3_Y = positionMap.get(c.getUrn().split("-")[0]).getY();
+
+        assert n3_X.equals(d3_X);
+        assert n3_Y.equals(d3_Y);
     }
 }
--- backend/src/test/resources/net/es/oscars/cuke/map_position.feature
+++ backend/src/test/resources/net/es/oscars/cuke/map_position.feature
@@ -1,22 +1,22 @@
-@wip @live
+@unit
 Feature: map positions
 
   I want to verify that I pass the correct map coordinates
 
   Scenario: All positions are different
     Given I load my positionMap from "config/test/map_positions/one.json"
-    Given I add nodes for devices "bois-cr1", "pnwg-cr5" and "kans-cr5"
+    Given I load devices from "config/test/map_positions/one_devices.json"
     Then I can verify my results
     Then I did not receive an exception
 
   Scenario: Multiple positions with same keys
     Given I load my positionMap from "config/test/map_positions/two.json"
-    Given I add nodes for devices "bois-cr1", "pnwg-cr5" and "pnwg-cr4"
+    Given I load devices from "config/test/map_positions/two_devices.json"
     Then I can verify my results
     Then I did not receive an exception
 
   Scenario: When one device matches multiple keys
     Given I load my positionMap from "config/test/map_positions/three.json"
-    Given I add nodes for devices "bois-cr1", "llnl-mr2" and "llnldc-rt5"
+    Given I load devices from "config/test/map_positions/three_devices.json"
     Then I can verify my results
     Then I did not receive an exception
