86eb1bec3e6f6a0f53453955f1cbcf9fcb44beda
Sartaj Singh Baveja <ssbaveja@es.net>
2019-09-25 11:49:22 -0700

    feat: [OS-72] Add History panel view to connection info (#345)
    * Add REST endpoint for event log per connectionId
    
    * Working history panel
    
    * Updates to styling
    
    * Code Refactoring
    
    * Remove log file
    
    * Remove unused variables

---
+++ frontend/src/main/js/components/details/detailsHistory.js
@@ -0,0 +1,109 @@
+import { action } from "mobx";
+import { inject, observer } from "mobx-react";
+import Moment from "moment";
+import React, { Component } from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
+import myClient from "../../agents/client";
+
+@inject("connsStore")
+@observer
+class DetailsHistory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
+        };
+    }
+    
+    componentWillMount() {
+        const pathConnectionId = this.props.connsStore.store.current.connectionId;
+        this.updateList(pathConnectionId);
+    }
+
+    componentWillUnmount() {
+        this.props.connsStore.clearCurrent();
+    }
+
+    refresh = () => {
+        const pathConnectionId = this.props.connsStore.store.current.connectionId;
+        this.updateList(pathConnectionId);
+    };
+
+    updateList = connectionId => {
+        myClient.submitWithToken("GET", "/api/log/conn/" + connectionId).then(
+            action(response => {
+                let eventLog = JSON.parse(response);
+                this.props.connsStore.setEventLog(eventLog);
+                this.setState({loading: false});
+            })
+        );
+    };
+
+    columns = [
+        {
+            accessor: "at",
+            Header: props => (
+                <div>
+                    <br />
+                    <b>Timestamp</b>
+                    <br />
+                    <br />
+                </div>
+            )
+          },
+          {
+            accessor: "description",
+            Header: props => (
+                <div>
+                    <br />
+                    <b>Description</b>
+                    <br />
+                    <br />
+                </div>
+            )
+          }
+    ];
+
+    render() {
+        let cs = this.props.connsStore;
+        const log = cs.store.eventLog;
+
+        if (log === undefined || log.length === 0) {
+            return (
+                <div>
+                    Loading...
+                </div>
+            )
+        } else {
+            let rows = [];
+            log.events.reverse().map(c => {
+                let row = {
+                    at: Moment(c.at).format('Y/MM/DD HH:mm:ss'),
+                    type: c.type,
+                    description: c.description
+                };
+                rows.push(row);
+            });
+
+            return (
+                <ReactTable
+                    data={rows}
+                    columns={this.columns}
+                    minRows={3}
+                    className="-striped -highlight"
+                    getTrProps={(state, rowInfo, column) => {
+                        return {
+                          style: {
+                            padding: '10px'
+                          }
+                        }
+                      }}
+                />
+            );
+        }
+    }
+}
+
+export default DetailsHistory;
