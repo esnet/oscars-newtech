a9b42dbc731ea75306b554e8d4bcb2e8ad9bfb36
Bruce A. Mah <bmah@es.net>
2019-10-01 08:20:02 -0700

    chore: [OS-78] Maintenance/resync of topology update code/docs (#347)
    * feat: [OS-78] Improve robustness of run.sh.
    Log error output, if any, from topo.py. Only stage output files if topo.py completed without error. Set ownership on staged output files. Changes originally made by @haniotak.
    
    * docs: [OS-78] Expand documentation for run.sh.
    
    * chore: [os-78] Remove a file that shouldn't be in the repo.

--- topo/run.sh
+++ topo/run.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 now=`date`
-echo "$now: starting topology update\n" >> /usr/local/esnet/topo-update/update.log
+echo "$now: starting topology update run" >> /usr/local/esnet/topo-update/update.log
 
-rm /usr/local/esnet/topo-update/output/*
+rm -f /usr/local/esnet/topo-update/output/*
+cd /usr/local/esnet/topo-update/
 
 /usr/local/esnet/topo-update/topo.py \
     --output-dir /usr/local/esnet/topo-update/output \
-    -v >> /usr/local/esnet/topo-update/update.log
+    -v 2>&1 >> /usr/local/esnet/topo-update/update.log
 
-mv /usr/local/esnet/topo-update/output/devices.json /usr/local/esnet/oscars-backend/config/topo/esnet-devices.json
-mv /usr/local/esnet/topo-update/output/adjcies.json /usr/local/esnet/oscars-backend/config/topo/esnet-adjcies.json
 
-now=`date`
-echo "$now: completed topology update\n" >> /usr/local/esnet/topo-update/update.log
+if [ $? -eq 0 ]
+then
+  now=`date`
+  mv /usr/local/esnet/topo-update/output/devices.json /usr/local/esnet/oscars-backend/config/topo/esnet-devices.json
+  mv /usr/local/esnet/topo-update/output/adjcies.json /usr/local/esnet/oscars-backend/config/topo/esnet-adjcies.json
+  chown nobody /usr/local/esnet/oscars-backend/config/topo/esnet-adjcies.json
+  chown nobody /usr/local/esnet/oscars-backend/config/topo/esnet-devices.json
+  echo "$now: completed topology update run" >> /usr/local/esnet/topo-update/update.log
+else
+  now=`date`
+  echo "$now: failed topology update run" >> /usr/local/esnet/topo-update/update.log
+  exit 1
+fi
+
