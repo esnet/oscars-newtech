9275bab2ff40c3371f4b77a1c31c1ce24bd75a4e
haniotak <haniotak@gmail.com>
2020-10-28 11:34:15 -0700

    compile fix

--- migration/src/main/java/net/es/oscars/timefix/TimestampExporter.java
+++ migration/src/main/java/net/es/oscars/timefix/TimestampExporter.java
@@ -72,7 +72,7 @@ public class TimestampExporter {
             out.println(sql);
 
             for (Event event : el.getEvents()) {
-                String evsql = "UPDATE event SET occured = "+i2p(event.getOccured())+" WHERE id = "+event.getId()+";";
+                String evsql = "UPDATE event SET occured = "+i2p(event.getOccurrence())+" WHERE id = "+event.getId()+";";
                 out.println(evsql);
             }
         }
